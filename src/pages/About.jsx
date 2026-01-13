bbca330ed9dd2f7cd697a8775557b97526750548
CoRine65 <coderine65@gmail.com>
2026-01-12 21:41:21 -0800

    feat: add basic About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,13 +2,43 @@ import { Link } from "react-router-dom";
 
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <main className="about">
 
-      <nav style={{ display: "flex", gap: "12px" }}>
+      <header className="about_header">
+        <h1> About Me </h1>
+      </header>
+
+      <section className="about_body">
+        <p>
+          Hello, my name is Catherine Navarro. I am a a software developer focused on 
+          building thoughtful, well-structured applications. I’m currently working primarily 
+          on the web with React and Rails, while steadily strengthening my foundation in computer 
+          science and system-level thinking. I care about clarity, clean architecture, and creating 
+          work that feels intentional.
+        </p>
+      </section>
+
+
+
+
+
+
+
+
+      <nav className="about_nav">
         <Link to="/">Home</Link>
+        <Link to="/Contact">    Contact</Link>
       </nav>
 
-    </div>
+
+    </main>
+
+
+
+
+
+
+
+    
   );
 }

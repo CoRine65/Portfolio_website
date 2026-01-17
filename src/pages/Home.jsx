dfd9fd9bced30b3fb889d11417a3d7d226f4c9cc
CoRine65 <coderine65@gmail.com>
2026-01-16 19:51:04 -0800

    feat: landing page scaffold: divs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom";
+const cardStyle = {
+  width: "200px",
+  height: "120px",
+  border: "1px dashed gray",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Catherine</h1>
       <nav style={{ display: "flex", gap: "12px" }}>
-        <Link to="/projects/sudoku">Sudoku Page</Link>
-        <Link to="/projects/bakery">Bakery Page</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+
+        
+        <div style={cardStyle}><Link to="/projects/sudoku">Sudoku Page</Link></div>
+        <div style={cardStyle}><Link to="/projects/bakery">Bakery Page</Link></div>
+        <div style={cardStyle}><Link to="/about">About</Link></div>
+        <div style={cardStyle}><Link to="/contact">Contact</Link></div>
+        
+        
+        
+        
       </nav>
 
     </div>
